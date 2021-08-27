79270dbba0ee3f00af571f9a6aa28e3002d6fc94
StidOfficial <stidofficiel@gmail.com>
2021-08-28 00:35:38 +0200

    ram0 shadow part of dram0

--- hw/arm/wpcm450_boards.c
+++ hw/arm/wpcm450_boards.c
@@ -87,18 +87,18 @@ static void npcm7xx_connect_dram(WPCM450State *soc, MemoryRegion *dram)
     memory_region_init_ram(dram2, NULL, "dram2", WPCM450_DRAM_MAX_SZ, &error_fatal);
     memory_region_init_ram(dram3, NULL, "dram3", WPCM450_DRAM_MAX_SZ, &error_fatal);
 
-    memory_region_add_subregion(smem, WPCM450_DRAM0_BA, dram);
+    memory_region_add_subregion_overlap(smem, WPCM450_DRAM0_BA, dram, 1);
 
     if(dram->size == WPCM450_1DRAM_CONFIG)
     {
         memory_region_init_alias(dram1, NULL, "dram1", dram, 0x0, WPCM450_DRAM_MIN_SZ);
-        memory_region_add_subregion_overlap(smem, WPCM450_DRAM1_BA, dram1, 0);
+        memory_region_add_subregion(smem, WPCM450_DRAM1_BA, dram1);
 
         memory_region_init_alias(dram2, NULL, "dram2", dram, 0x0, WPCM450_DRAM_MIN_SZ);
-        memory_region_add_subregion_overlap(smem, WPCM450_DRAM2_BA, dram2, 0);
+        memory_region_add_subregion(smem, WPCM450_DRAM2_BA, dram2);
 
         memory_region_init_alias(dram3, NULL, "dram3", dram, 0x0, WPCM450_DRAM_MIN_SZ);
-        memory_region_add_subregion_overlap(smem, WPCM450_DRAM3_BA, dram3, 0);
+        memory_region_add_subregion(smem, WPCM450_DRAM3_BA, dram3);
     }
 
     object_property_set_link(OBJECT(soc), "dram-mr", OBJECT(dram),
